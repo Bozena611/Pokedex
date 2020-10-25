d75a07a7b892634713bf843b989c99c788f09242
Bozena611 <bozena@barcelonacodeschool.com>
2020-10-25 14:06:41 +0100

    exercise E

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 
+function Logo() {
+  return (
+    <header>
+      <h1>Welcome to Pokedex</h1>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" />
+    </header>
+    )
+}
+
+function BestPokemon(){
+  return <p>My favourite Pokemon is Squirtle</p>
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <h1>Welcome to Pokedex</h1>
-        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" />
-      </header>
+      <Logo />
+      <BestPokemon />
     </div>
   )  
 }
