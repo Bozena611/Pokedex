ef67339f513bc6fcbab0bf567f174e7eeef0f3cf
Bozena611 <bozena@barcelonacodeschool.com>
2020-10-27 23:17:25 +0100

    week 2 exerc-E 1-6

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,15 +3,21 @@ import React, {useState} from "react";
 const CaughtPokemon = (props) => {
   console.log("props date", props);
 
-  const [caught, setCaught] = useState(0);
+  const [caught, setCaught] = useState([]);
 
   const catchPokemon = () => {
-  	setCaught(caught + 1)
+  	//use concat instead of push
+  	setCaught(caught.concat("Pokemon"));
   }
 
   return (
   	<div>
-  		<p>Caught {caught} Pokemon on {props.date}</p>
+  		<p>Caught {caught.length} Pokemon on {props.date}</p>
+  		<ul>
+  		{caught.map((element, index)=>{
+  			return <li key={index}>{element}</li>
+  		})}
+  		</ul>
   		<button onClick={catchPokemon}>Click me</button>
   	</div>
   )
