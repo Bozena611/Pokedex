157f07178bf2d52800c315eec961a5ce2bdcb494
Bozena611 <bozena@barcelonacodeschool.com>
2020-10-27 22:23:39 +0100

    week 2 exerc-A

--- src/Logo.js
+++ src/Logo.js
@@ -4,10 +4,15 @@ import React from "react";
 const Logo = (props) => {
   console.log("props", props)
 
+
+  const logWhenClicked = () => {
+  	console.log("You clicked on the image");
+  }
+
   return (
     <header>
       <h1>Welcome to {props.appName}</h1>
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Squirtle" />
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Squirtle" onClick={logWhenClicked}/>
     </header>
     )
 }
