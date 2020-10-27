dbd656091a5a8ee4dc46fea66828eb66169b3fe3
Bozena611 <bozena@barcelonacodeschool.com>
2020-10-27 22:33:13 +0100

    week 2 exerc B

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import CaughtPokemon from "./CaughtPokemon";
 function App() {
   let abilities = ['Anticipation', 'Adaptability', 'Run-Away'];
   let date = new Date().toLocaleDateString();
+
+  const logWhenClicked = () => {
+  	console.log("You clicked on the image");
+  }
   return (
     <div>
-      <Logo appName="Pokedex"/>
+      <Logo appName="Pokedex" handleClick={logWhenClicked}/>
       <BestPokemon abilities={abilities}/>
       <CaughtPokemon date={date}/>
     </div>
--- src/Logo.js
+++ src/Logo.js
@@ -3,16 +3,11 @@ import React from "react";
 
 const Logo = (props) => {
   console.log("props", props)
-
-
-  const logWhenClicked = () => {
-  	console.log("You clicked on the image");
-  }
-
+  
   return (
     <header>
       <h1>Welcome to {props.appName}</h1>
-      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Squirtle" onClick={logWhenClicked}/>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png" alt="Squirtle" onClick={props.handleClick}/>
     </header>
     )
 }
