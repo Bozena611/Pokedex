ec5e3510a3bb0948db3c870d8880585bf5c1e5b8
Bozena611 <bozena@barcelonacodeschool.com>
2020-10-27 22:57:51 +0100

    week 2 exerc D

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 const CaughtPokemon = (props) => {
   console.log("props date", props);
-  return <p>Caught 0 Pokemon on {props.date}</p>
+
+  const [caught, setCaught] = useState(0);
+
+  const catchPokemon = () => {
+  	setCaught(caught + 1)
+  }
+
+  return (
+  	<div>
+  		<p>Caught {caught} Pokemon on {props.date}</p>
+  		<button onClick={catchPokemon}>Click me</button>
+  	</div>
+  )
 }
 
 export default CaughtPokemon;
