3ce7891d5a8a061df7e8b58673f404d72484a225
Bozena611 <bozena@barcelonacodeschool.com>
2020-11-08 22:06:54 +0100

    react week 3 ex-G

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -4,12 +4,20 @@ const CaughtPokemon = (props) => {
   console.log("props date", props);
 
   const [caught, setCaught] = useState([]);
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
 
   const catchPokemon = () => {
   	//use concat instead of push
-    const list = ["Pikachu", "Eevee", "Raichu", "Glaceon"]
-    const random = list[Math.floor(Math.random() * list.length)];
-    setCaught(caught.concat(random));
+    //const list = ["Pikachu", "Eevee", "Raichu", "Glaceon"]
+    //const random = list[Math.floor(Math.random() * list.length)];
+    if(pokemonNameInput) {
+      setCaught(caught.concat(pokemonNameInput));
+      setPokemonNameInput("");
+    }
+  }
+
+  const handleInputChange = (event) => {
+    setPokemonNameInput(event.target.value);
   }
 
   return (
@@ -20,6 +28,11 @@ const CaughtPokemon = (props) => {
   			return <li key={index}>{element}</li>
   		})}
   		</ul>
+      <input 
+        type="text" 
+        value={pokemonNameInput}
+        onChange={handleInputChange}
+      />
   		<button onClick={catchPokemon}>Click me</button>
   	</div>
   )
